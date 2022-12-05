3b79a66e81b4c5c6937155a4fb407bcd5aaefa59
Ahmad Reza <hungry.dev39@gmail.com>
2022-12-05 16:11:39 +0700

    chore: remove logging

--- router.go
+++ router.go
@@ -4,12 +4,10 @@ import (
 	"hungrydev39/todo-list-challenge/controllers"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 )
 
 func setupRouter() *chi.Mux {
 	r := chi.NewRouter()
-	r.Use(middleware.Logger)
 
 	r.Get("/activity-groups", controllers.AllActivity)
 	r.Get("/activity-groups/{id}", controllers.ShowActivity)
